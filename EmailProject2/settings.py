1db6bbc3f4a2e0cc98740b61d28bedf870b6342d
Adi19471 <akumatha@gmail.com>
2021-09-01 23:46:27 +0530

    EMAIL PROCESS

--- EmailProject2/settings.py
+++ EmailProject2/settings.py
@@ -137,4 +137,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
 EMAIL_HOST_USER = 'djangomvtadi@gmail.com'  
-EMAIL_HOST_PASSWORD = 'Bigbasket@019471'  
+EMAIL_HOST_PASSWORD = 'Bigbasket@s019471'  
